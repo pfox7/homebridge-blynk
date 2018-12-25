fdcbd9cf3906b44a3d61066e747d8cb1d5b7a02e
pfox7 <46032722+pfox7@users.noreply.github.com>
2018-12-25 21:31:36 +0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -54,143 +54,146 @@ const pollerupdate_1 = require("./pollerupdate");
 const defaultPollerPeriod = 1;
 let Accessory, Service, Characteristic, UUIDGen;
 class Config {
-    constructor() {
-        this.name = "";
-        this.serverurl = "";
-        this.token = "";
-        this.pollerperiod = "";
-        this.accessories = "";
-    }
+	constructor() {
+		this.name = "";
+		this.serverurl = "";
+		this.token = "";
+		this.pollerperiod = "";
+		this.accessories = "";
+	}
 }
 class Blynk {
-    constructor(log, config, api) {
-        this.log = log;
-        this.api = api;
-        this.accessories = new Map();
-        this.updateSubscriptions = new Array();
-        this.config = config;
-        let pollerPeriod = this.config.pollerperiod ? parseInt(this.config.pollerperiod) : defaultPollerPeriod;
-        if (isNaN(pollerPeriod) || pollerPeriod < 1 || pollerPeriod > 100)
-            pollerPeriod = defaultPollerPeriod;
-        this.poller = new pollerupdate_1.Poller(this, pollerPeriod, Service, Characteristic);
-        this.api.on('didFinishLaunching', this.didFinishLaunching.bind(this));
-    }
-    didFinishLaunching() {
-        this.log('didFinishLaunching.', '');
-        this.LoadAccessories(this.config.accessories);
-    }
-    configureAccessory(accessory) {
-        this.log("Configured Accessory: ", accessory.displayName);
-        for (let s = 0; s < accessory.services.length; s++) {
-            let service = accessory.services[s];
-            for (let i = 0; i < service.characteristics.length; i++) {
-                let characteristic = service.characteristics[i];
-                if (characteristic.props.needsBinding)
-                    this.bindCharacteristicEvents(characteristic, service);
-            }
-        }
-        this.accessories.set(accessory.context.uniqueSeed, accessory);
-        accessory.reachable = true;
-    }
-    LoadAccessories(accessories) {
-        this.log('Loading accessories', '');
-        if (accessories == null || accessories.length == 0) {
-            return;
-        }
-        accessories.map((s, i, a) => {
-            this.addAccessory(blynkaccessory_1.BlynkAccessory.createBlynkAccessory(s, Accessory, Service, Characteristic, this));
-        });
-        // Remove not reviewd accessories: cached accessories no more present in config.json
-        let aa = this.accessories.values(); // Iterator for accessories, key is the uniqueseed
-        for (let a of aa) {
-            if (!a.reviewed) {
-                this.removeAccessory(a);
-            }
-        }
-        // Start the poller update mechanism
-        this.poller.poll();
-    }
-    addAccessory(blynkAccessory) {
-        if (blynkAccessory == undefined)
-            return;
-        let uniqueSeed = blynkAccessory.name;
-        let isNewAccessory = false;
-        let a = this.accessories.get(uniqueSeed);
-        if (a == null) {
-            isNewAccessory = true;
-            let uuid = UUIDGen.generate(uniqueSeed);
-            a = new Accessory(blynkAccessory.name, uuid); // Create the HAP accessory
-            a.context.uniqueSeed = uniqueSeed;
-            this.accessories.set(uniqueSeed, a);
-        }
-        blynkAccessory.setAccessory(a);
-        // init accessory
-        blynkAccessory.initAccessory();
-        // Remove services existing in HomeKit, device no more present in Blynk
-        blynkAccessory.removeNoMoreExistingServices();
-        // Add services present in Blynk and not existing in Homekit accessory
-        blynkAccessory.addNewServices(this);
-        // Register or update platform accessory
-        blynkAccessory.registerUpdateAccessory(isNewAccessory, this.api);
-        this.log("Added/changed accessory: ", blynkAccessory.name);
-    }
-    removeAccessory(accessory) {
-        this.log('Remove accessory', accessory.displayName);
-        this.api.unregisterPlatformAccessories(blynkaccessory_1.pluginName, blynkaccessory_1.platformName, [accessory]);
-        this.accessories.delete(accessory.context.uniqueSeed);
-    }
-    bindCharacteristicEvents(characteristic, service) {
-        characteristic.on('set', (value, callback, context) => {
-            this.setCharacteristicValue(value, callback, context, characteristic, service);
-        });
-        characteristic.on('get', (callback) => {
-            this.getCharacteristicValue(callback, characteristic, service);
-        });
-        this.subscribeUpdate(service, characteristic);
-    }
-    subscribeUpdate(service, characteristic) {
-        this.updateSubscriptions.push({ 'service': service, 'characteristic': characteristic });
-    }
-    setCharacteristicValue(value, callback, context, characteristic, service) {
-        if (context !== 'fromPoller' && context !== 'fromSetValue') {
-            let params = service.subtype.split("-"); // params[0]: name, params[1]: widget, params[2]: pin, params[3]: mode
-            let name = params[0];
-            let widget = params[1];
-            let pinString = params[2];
-            let mode = params[3];
-            this.log("Setting value to device: ", `${name}, parameter: ${characteristic.displayName}, value: ${value}`);
-            switch (widget) {
-                case "Switch":
-                    let v = value ? "1" : "0";
-                    request(this.config.serverurl + '/' + this.config.token + '/update/' + pinString + "?value=" + v, function (error, response, body) {
-                        if (!error && mode == "PUSH") {
-                            // In order to behave like a push button reset the status to off
-                            setTimeout(function () {
-                                characteristic.setValue(false, undefined, 'fromSetValue');
-                            }, 100);
-                        }
-                    });
-                    break;
-                default:
-                    break;
-            }
-        }
-        callback();
-    }
-    getCharacteristicValue(callback, characteristic, service) {
-        this.log("Getting value from device: ", `parameter: ${characteristic.displayName}`);
-        let params = service.subtype.split("-"); // params[0]: name, params[1]: widget, params[2]: pin, params[3]: mode
-        let name = params[0];
-        let widget = params[1];
-        let pinString = params[2];
-        pollerupdate_1.getBlynkvalue(name, widget, pinString, callback, characteristic, Characteristic, this);
-    }
+	constructor(log, config, api) {
+		this.log = log;
+		this.api = api;
+		this.accessories = new Map();
+		this.updateSubscriptions = new Array();
+		this.config = config;
+		let pollerPeriod = this.config.pollerperiod ? parseInt(this.config.pollerperiod) : defaultPollerPeriod;
+		if (isNaN(pollerPeriod) || pollerPeriod < 1 || pollerPeriod > 100)
+			pollerPeriod = defaultPollerPeriod;
+		this.poller = new pollerupdate_1.Poller(this, pollerPeriod, Service, Characteristic);
+		this.api.on('didFinishLaunching', this.didFinishLaunching.bind(this));
+	}
+	didFinishLaunching() {
+		this.log('didFinishLaunching.', '');
+		this.LoadAccessories(this.config.accessories);
+	}
+	configureAccessory(accessory) {
+		this.log("Configured Accessory: ", accessory.displayName);
+		for (let s = 0; s < accessory.services.length; s++) {
+			let service = accessory.services[s];
+			for (let i = 0; i < service.characteristics.length; i++) {
+				let characteristic = service.characteristics[i];
+				if (characteristic.props.needsBinding)
+					this.bindCharacteristicEvents(characteristic, service);
+			}
+		}
+		this.accessories.set(accessory.context.uniqueSeed, accessory);
+		accessory.reachable = true;
+	}
+	LoadAccessories(accessories) {
+		this.log('Loading accessories', '');
+		if (accessories == null || accessories.length == 0) {
+			return;
+		}
+		accessories.map((s, i, a) => {
+			this.addAccessory(blynkaccessory_1.BlynkAccessory.createBlynkAccessory(s, Accessory, Service, Characteristic, this));
+		});
+		// Remove not reviewd accessories: cached accessories no more present in config.json
+		let aa = this.accessories.values(); // Iterator for accessories, key is the uniqueseed
+		for (let a of aa) {
+			if (!a.reviewed) {
+				this.removeAccessory(a);
+			}
+		}
+		// Start the poller update mechanism
+		this.poller.poll();
+	}
+	addAccessory(blynkAccessory) {
+		if (blynkAccessory == undefined)
+			return;
+		let uniqueSeed = blynkAccessory.name;
+		let isNewAccessory = false;
+		let a = this.accessories.get(uniqueSeed);
+		if (a == null) {
+			isNewAccessory = true;
+			let uuid = UUIDGen.generate(uniqueSeed);
+			a = new Accessory(blynkAccessory.name, uuid); // Create the HAP accessory
+			a.context.uniqueSeed = uniqueSeed;
+			this.accessories.set(uniqueSeed, a);
+		}
+		blynkAccessory.setAccessory(a);
+		// init accessory
+		blynkAccessory.initAccessory();
+		// Remove services existing in HomeKit, device no more present in Blynk
+		blynkAccessory.removeNoMoreExistingServices();
+		// Add services present in Blynk and not existing in Homekit accessory
+		blynkAccessory.addNewServices(this);
+		// Register or update platform accessory
+		blynkAccessory.registerUpdateAccessory(isNewAccessory, this.api);
+		this.log("Added/changed accessory: ", blynkAccessory.name);
+	}
+	removeAccessory(accessory) {
+		this.log('Remove accessory', accessory.displayName);
+		this.api.unregisterPlatformAccessories(blynkaccessory_1.pluginName, blynkaccessory_1.platformName, [accessory]);
+		this.accessories.delete(accessory.context.uniqueSeed);
+	}
+	bindCharacteristicEvents(characteristic, service) {
+		characteristic.on('set', (value, callback, context) => {
+			this.setCharacteristicValue(value, callback, context, characteristic, service);
+		});
+		characteristic.on('get', (callback) => {
+			this.getCharacteristicValue(callback, characteristic, service);
+		});
+		this.subscribeUpdate(service, characteristic);
+	}
+	subscribeUpdate(service, characteristic) {
+		this.updateSubscriptions.push({ 'service': service, 'characteristic': characteristic });
+	}
+	setCharacteristicValue(value, callback, context, characteristic, service) {
+		if (context !== 'fromPoller' && context !== 'fromSetValue') {
+			let params = service.subtype.split("-"); // params[0]: name, params[1]: widget, params[2]: pin, params[3]: token, params[4]: mode
+			let name = params[0];
+			let widget = params[1];
+			let pinString = params[2];
+			let token = params[3];
+			let mode = params[4];
+			this.log("Setting value to device: ", `${name}, parameter: ${characteristic.displayName}, value: ${value}`);
+			switch (widget) {
+				case "Switch":
+					let v = value ? "1" : "0";
+					request(this.config.serverurl + '/' + this.config.token + '/update/' + pinString + "?value=" + v, function (error, response, body) {
+						if (!error && mode == "PUSH") {
+							// In order to behave like a push button reset the status to off
+							setTimeout(function () {
+								characteristic.setValue(false, undefined, 'fromSetValue');
+							}, 100);
+						}
+					});
+					break;
+				default:
+					break;
+			}
+		}
+		callback();
+	}
+	getCharacteristicValue(callback, characteristic, service) {
+		this.log("Getting value from device: ", `parameter: ${characteristic.displayName}`);
+		let params = service.subtype.split("-"); // params[0]: name, params[1]: widget, params[2]: pin, params[3]: token, params[4]: mode
+		let name = params[0];
+		let widget = params[1];
+		let pinString = params[2];
+		let token = params[3];
+		let mode = params[4];
+		pollerupdate_1.getBlynkvalue(name, widget, pinString, token, callback, characteristic, Characteristic, this);
+	}
 }
 module.exports = function (homebridge) {
-    Accessory = homebridge.platformAccessory;
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-    UUIDGen = homebridge.hap.uuid;
-    homebridge.registerPlatform(blynkaccessory_1.pluginName, blynkaccessory_1.platformName, Blynk, true);
+	Accessory = homebridge.platformAccessory;
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+	UUIDGen = homebridge.hap.uuid;
+	homebridge.registerPlatform(blynkaccessory_1.pluginName, blynkaccessory_1.platformName, Blynk, true);
 };
 //# sourceMappingURL=index.js.map
