0bb04b4f6ccbf34275bae6f38771581bfb954b26
pfox7 <46032722+pfox7@users.noreply.github.com>
2019-01-08 13:28:08 +0300

    Update pollerupdate.ts

--- src/pollerupdate.ts
+++ src/pollerupdate.ts
@@ -41,70 +41,73 @@ export class Poller {
 			return;
 		}
 		this.pollingUpdateRunning = true;
-    
-        this.platform.config.accessories.map((s, i, a) => {
+	
+		this.platform.config.accessories.map((s, i, a) => {
 			this.updateAccessory(s);
 		});
 
-        this.pollingUpdateRunning = false;
+		this.pollingUpdateRunning = false;
 		setTimeout( () => { this.poll()}, this.pollerPeriod * 1000);
 	}
 	
 	updateAccessory(accessory) {
 		for (let i = 0; i < this.platform.updateSubscriptions.length; i++) {
-            let subscription = this.platform.updateSubscriptions[i];
-            let service = subscription.service;
-            let params = service.subtype.split("-"); // params[0]: name, params[1]: widget, params[2]: pin, params[3]: mode
-            let name = params[0];
-            let widget = params[1];        
-            let pinString = params[2];
-            let token = params[3];
+			let subscription = this.platform.updateSubscriptions[i];
+			let service = subscription.service;
+			let params = service.subtype.split("-"); // params[0]: name, params[1]: widget, params[2]: pin, params[3]: mode, params[4]: token
+			let name = params[0];
+			let widget = params[1];        
+			let pinString = params[2];
+			let token = params[3];
+			let mode = params[4];
 			if (name == accessory.name) {
-                getBlynkvalue(name, widget, pinString, token, null, subscription.characteristic, this.hapCharacteristic, this.platform);
+				getBlynkvalue(name, widget, pinString, token, mode, null, subscription.characteristic, this.hapCharacteristic, this.platform);
 			}
 		}
-    }
+	}
 
 }
 
-export function getBlynkvalue(name, widget, pinString, token, callback, characteristic, Characteristic, platform) {
-    request(platform.config.serverurl + '/' + token + '/get/' + pinString, function (error, response, body) {
-        // console.log('Status:', response.statusCode);
-        // console.log('Headers:', JSON.stringify(response.headers));
-        // console.log('Response:', body);
-        function returnValue(r, callback, characteristic) {
-            if (callback) {
-				platform.log("Getting value for device: ", `${name}  parameter: ${characteristic.displayName}, value: ${r}`);
-                callback(undefined, r);
-            } else {
-				platform.log("Updating value for device: ", `${name}  parameter: ${characteristic.displayName}, value: ${r}`);
-                characteristic.updateValue(r);
-            }
-        }
-        switch (widget) {
-            case "Switch":
-                returnValue((body == "[\"1\"]"), callback, characteristic);
-                break;
-            case "ContactSensor":
-                returnValue((body == "[\"1\"]") ? Characteristic.ContactSensorState.CONTACT_DETECTED : Characteristic.ContactSensorState.CONTACT_NOT_DETECTED, callback, characteristic);
-                break;
-            case "TemperatureSensor":
-                returnValue(parseFloat(JSON.parse(body)), callback, characteristic);
-                break;
-            case "HumiditySensor":
-                returnValue(parseFloat(JSON.parse(body)), callback, characteristic);
-                break;
-            case "MotionSensor":
-                returnValue((body == "[\"1\"]"), callback, characteristic);
-                break;
-            case "SmokeSensor":
-                returnValue((body == "[\"1\"]") ? Characteristic.SmokeDetected.SMOKE_DETECTED : Characteristic.SmokeDetected.SMOKE_NOT_DETECTED, callback, characteristic);
-                break;
-            case "LightSensor":
-                returnValue(parseFloat(JSON.parse(body)), callback, characteristic);
-                break;
-            default:
-                break;
-        }
-    })
+export function getBlynkvalue(name, widget, pinString, token, mode, callback, characteristic, Characteristic, platform) {
+	request(platform.config.serverurl + '/' + token + '/get/' + pinString, function (error, response, body) {
+		if (!error && response.statusCode == 200 && body != undefined) {
+			// console.log('Status:', response.statusCode);
+			// console.log('Headers:', JSON.stringify(response.headers));
+			// console.log('Response:', body);
+			function returnValue(r, callback, characteristic) {
+				if (callback) {
+					platform.log("Getting value for device: ", `${name}  parameter: ${characteristic.displayName}, value: ${r}`);
+					callback(undefined, r);
+				} else {
+					platform.log("Updating value for device: ", `${name}  parameter: ${characteristic.displayName}, value: ${r}`);
+					characteristic.updateValue(r);
+				}
+			}
+			switch (widget) {
+				case "Switch":
+					returnValue(((mode == "REVERSESWITCH" || mode == "REVERSEPUSH") ? (body != "[\"1\"]") : (body == "[\"1\"]")), callback, characteristic);
+					break;
+				case "ContactSensor":
+					returnValue((body == "[\"1\"]") ? Characteristic.ContactSensorState.CONTACT_DETECTED : Characteristic.ContactSensorState.CONTACT_NOT_DETECTED, callback, characteristic);
+					break;
+				case "TemperatureSensor":
+					returnValue(parseFloat(JSON.parse(body)), callback, characteristic);
+					break;
+				case "HumiditySensor":
+					returnValue(parseFloat(JSON.parse(body)), callback, characteristic);
+					break;
+				case "MotionSensor":
+					returnValue((body == "[\"1\"]"), callback, characteristic);
+					break;
+				case "SmokeSensor":
+					returnValue((body == "[\"1\"]") ? Characteristic.SmokeDetected.SMOKE_DETECTED : Characteristic.SmokeDetected.SMOKE_NOT_DETECTED, callback, characteristic);
+					break;
+				case "LightSensor":
+					returnValue(parseFloat(JSON.parse(body)), callback, characteristic);
+					break;
+				default:
+					break;
+			}
+		}
+	})
 }
